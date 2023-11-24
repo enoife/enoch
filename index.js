0d1d07c554f47bf3ae2b80961090e56b252f72a4
enoch <ifemideenoch96@gamil.com>
2023-11-23 22:08:00 -0800

    newsc

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function textChange(){
   document.getElementById('text').innerHTML= "Get Portfolio at Github enoife"
 }
+
+function imgChange(){
+  document.getElementById('pic').innerHTML= " Very happy to work with You"
+}
